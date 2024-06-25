6c902f4137c5b2f9c3048f4f9918279c963c77fd
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-06-24 21:17:41 -0500

    Getting started With REST API

---
+++ 1.GettingStarted/Movies.Api/Mappings/ContractMapping.cs
@@ -0,0 +1,19 @@
+﻿using Movies.Contracts.Requests;
+using Movies.Application.Models;
+
+namespace Movies.Api.Mappings
+{
+    public static class ContractMapping
+    {
+        public static Movie MapToMovie(this CreateMovieRequest request) {
+           return  new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList(),
+            };
+
+        }
+    }
+}
